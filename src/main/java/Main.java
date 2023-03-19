e988969874c5cd056eb65c7ee9eb66e3d6949c74
Shashank <shashaanktyagi@gmail.com>
2023-03-19 16:27:35 +0530

    enable port
    enable port
    
    enable port
    
    enable port
    
    enable port
    
    pass 1st stage
    
    Added ping response
    
    Fixed  ping response
    
    Fixed  ping response
    
    Fixed  ping response
    
    Fixed  test 2
    
    Fixed using PrintWriter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -6,26 +9,29 @@ public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
-
     //  Uncomment this block to pass the first stage
-    //    ServerSocket serverSocket = null;
-    //    Socket clientSocket = null;
-    //    int port = 6379;
-    //    try {
-    //      serverSocket = new ServerSocket(port);
-    //      serverSocket.setReuseAddress(true);
-    //      // Wait for connection from client.
-    //      clientSocket = serverSocket.accept();
-    //    } catch (IOException e) {
-    //      System.out.println("IOException: " + e.getMessage());
-    //    } finally {
-    //      try {
-    //        if (clientSocket != null) {
-    //          clientSocket.close();
-    //        }
-    //      } catch (IOException e) {
-    //        System.out.println("IOException: " + e.getMessage());
-    //      }
-    //    }
+    ServerSocket serverSocket = null;
+    Socket clientSocket = null;
+    int port = 6379;
+    System.out.println("Opening port: " + 6379);
+    try {
+      serverSocket = new ServerSocket(port);
+      serverSocket.setReuseAddress(true);
+      // Wait for connection from client.
+      clientSocket = serverSocket.accept();
+      PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+      out.print("+PONG\r\n");
+      out.flush();
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    } finally {
+      try {
+        if (clientSocket != null) {
+          clientSocket.close();
+        }
+      } catch (IOException e) {
+        System.out.println("IOException: " + e.getMessage());
+      }
+    }
   }
 }
