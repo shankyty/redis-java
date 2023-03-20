10e64c593db9c08abcf53f9b4e75158848de90f0
Shashank <shashaanktyagi@gmail.com>
2023-03-20 17:13:38 +0530

    Added support for multilpe ping

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
-    //  Uncomment this block to pass the first stage
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-    int port = 6379;
-    System.out.println("Opening port: " + 6379);
-    try {
-      serverSocket = new ServerSocket(port);
-      serverSocket.setReuseAddress(true);
-      // Wait for connection from client.
-      clientSocket = serverSocket.accept();
-      PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-      out.print("+PONG\r\n");
-      out.flush();
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
+        //  Uncomment this block to pass the first stage
+        int port = 6379;
+        System.out.println("Opening port: " + port);
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+            try (Socket clientSocket = serverSocket.accept()) {
+                InputStream in = clientSocket.getInputStream();
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(clientSocket.getOutputStream()));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+
+                    System.out.println("line = " + line);
+                    if ("ping".equals(line)) {
+                        writer.write("+PONG\r\n");
+                        writer.flush();
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+            e.printStackTrace();
         }
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
-      }
     }
-  }
 }
