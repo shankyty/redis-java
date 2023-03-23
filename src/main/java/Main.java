67355af101a065ff5bf5f2498217233544bb316e
Shashank <shashaanktyagi@gmail.com>
2023-03-23 18:45:43 +0530

    Added support for multilpe clients

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,41 +1,82 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
+
     public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        System.out.println("Logs from your program will appear here!");
-        //  Uncomment this block to pass the first stage
-        int port = 6379;
-        System.out.println("Opening port: " + port);
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
+        try(ExecutorService taskExecutor = Executors.newCachedThreadPool(Executors
+                .defaultThreadFactory())) {
+            // You can use print statements as follows for debugging, they'll be visible when running tests.
+            System.out.println(namePrefix() + "Logs from your program will appear here!");
+            //  Uncomment this block to pass the first stage
+            int port = 6379;
+            System.out.println(namePrefix() + "Opening port: " + port);
+            try (ServerSocket serverSocket = new ServerSocket(port)) {
 
-            serverSocket.setReuseAddress(true);
-            // Wait for connection from client.
-            try (Socket clientSocket = serverSocket.accept()) {
-                InputStream in = clientSocket.getInputStream();
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(clientSocket.getInputStream()));
-                BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(clientSocket.getOutputStream()));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-
-                    System.out.println("line = " + line);
-                    if ("ping".equals(line)) {
-                        writer.write("+PONG\r\n");
-                        writer.flush();
-                    }
+                serverSocket.setReuseAddress(true);
+                // Wait for connection from client.
+                while (!serverSocket.isClosed()) {
+                    Socket clientSocket = serverSocket.accept();
+                    taskExecutor.submit(getReplyTask(clientSocket));
                 }
             } catch (IOException e) {
-                System.out.println("IOException: " + e.getMessage());
-                e.printStackTrace();
+                logException(e);
             }
-        } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-            e.printStackTrace();
         }
+    }
+    public static Runnable getReplyTask(Socket clientSocket) {
+        return () -> {
+            try(AutoCloseable ignored = setupLogger(clientSocket)) {
+                try {
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(clientSocket.getInputStream()));
+                    BufferedWriter writer = new BufferedWriter(
+                            new OutputStreamWriter(clientSocket.getOutputStream()));
+
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+
+                        System.out.println(namePrefix() + "line = " + line);
+                        if ("ping".equals(line)) {
+                            writer.write("+PONG\r\n");
+                            writer.flush();
+                        }
+                    }
+                } catch (IOException e) {
+                    logException(e);
+                } finally {
+                    try {
+                        System.out.println(namePrefix() + "closing");
+                        clientSocket.close();
+                    } catch (IOException e) {
+                        logException(e);
+                    }
+                }
+            } catch (Exception e) {
+                logException(e);
+            }
+        };
+    }
+
+    private static void logException(Exception e) {
+        System.out.println(namePrefix() + e.getClass().getSimpleName() + ": " + e.getMessage());
+        e.printStackTrace(System.out);
+    }
+
+    private static void updateName(String name) {
+        Thread.currentThread().setName(name);
+    }
+
+    private static AutoCloseable setupLogger(Socket clientSocket) {
+        String oldName = Thread.currentThread().getName();
+        updateName(clientSocket.getRemoteSocketAddress().toString());
+        return () -> updateName(oldName);
+    }
+
+    private static String namePrefix(){
+        return Thread.currentThread().getName() + ": ";
     }
 }
